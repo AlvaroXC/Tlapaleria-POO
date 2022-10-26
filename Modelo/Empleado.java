d3ef7fc106ab33a4653c4a5df1f0e8b9f7130810
AndreaTorresP <A18003912@alumnos.uady.mx>
2022-10-26 14:31:30 -0500

    add paquetes

---
+++ Modelo/Empleado.java
@@ -0,0 +1,56 @@
+package Modelo;
+
+public abstract class Empleado implements Pagar{
+    private String codEmpleado;
+    private String nombre;
+    private String apPat;
+    private String apMat;
+
+    //Constructor
+    public Empleado(String codEmpleado, String nombre, String apPat, String apMat){
+        this.codEmpleado = codEmpleado;
+        this.nombre = nombre;
+        this.apPat = apPat;
+        this.apMat = apMat;
+    }
+
+    //Métodos set
+    public void setCodEmpleado(String codEmpleado){
+        this.codEmpleado = codEmpleado;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setApPaterno(String apPat){
+        this.apPat = apPat;
+    }
+
+    public void setApMaterno(String apMat){
+        this.apMat = apMat;
+    }
+    
+    //Métodos get
+    public String getCodEmpleado(){
+        return codEmpleado;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApPat() {
+        return apPat;
+    }
+
+    public String getApMat() {
+        return apMat;
+    }
+    
+    //Método toString
+    @Override
+    public String toString() {
+        return "Código de empleado: " + getCodEmpleado() + "\nNombre: " + getNombre() + " " + getApPat() + " " + getApMat();
+    }
+}
---
+++ Modelo/Fecha.java
@@ -0,0 +1,46 @@
+package Modelo;
+
+public class Fecha {
+    private int dia;
+    private int mes;
+    private int anio;
+
+    //Constructor
+    public Fecha(int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    //Método establecer
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+
+    public void setAnio(int anio){
+        this.anio = anio;
+    }
+
+    //Método obtener
+    public int getDia(){
+        return dia;
+    }
+
+    public int getMes(){
+        return mes;
+    }
+
+    public int getAnio(){
+        return anio;
+    }
+
+    //Método toString
+    @Override
+    public String toString(){
+        return  getDia() + "/" + getMes() + "/" + getAnio();
+    }
+}
