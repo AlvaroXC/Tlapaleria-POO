e2aeb4b33a1691cfdca4d839753c5048e01c6438
JonathanQu3v3do <116295125+JonathanQu3v3do@users.noreply.github.com>
2022-11-03 13:59:02 -0600

    Update Cliente.java

--- Modelo/Cliente.java
+++ Modelo/Cliente.java
@@ -1,30 +1,65 @@
 package Modelo;
 
-public abstract class Cliente extends Empleado {
-    /* 
+public abstract class Cliente  {
+     
     private String codCliente;
     private String nombre;
     private String apPat;
     private String apMat;
-    */
     private String rfc;
-
-     
-    public Cliente(String codEmpleado, String nombre, String apPat, String apMat, String rfc) {
-        super(codEmpleado, nombre, apPat, apMat);
+     //constructores
+    
+     public cliente(String codCliente, String nombre, String apPat, String apMat, String rfc) {
+        this.codCliente = codCliente;
+        this.nombre = nombre;
+        this.apPat = apPat;
+        this.apMat = apMat;
         this.rfc = rfc;
     }
     
-    public void setRFC(String rfc) {
-        this.rfc = rfc;
+     //get and set
+
+    public String getCodCliente() {
+        return codCliente;
     }
 
-    public String getRFC() {
+    public void setCodCliente(String codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPat() {
+        return apPat;
+    }
+
+    public void setApPat(String apPat) {
+        this.apPat = apPat;
+    }
+
+    public String getApMat() {
+        return apMat;
+    }
+
+    public void setApMat(String apMat) {
+        this.apMat = apMat;
+    }
+
+    public String getRfc() {
         return rfc;
     }
 
-    @Override
-    public String toString(){
-        return super.toString()+"\nRFC: "+ getRFC();
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
     }
+    
+    
+    
+  
 }
